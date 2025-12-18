eddca6fae8600804056d6c559a78cefb247a785a
jip <83951137+jipgg@users.noreply.github.com>
2025-12-18 23:31:07 +0100

    add qol Unexpected function

--- src/Precursor/Expected.cs
+++ src/Precursor/Expected.cs
@@ -4,6 +4,11 @@ using static MethodImplOptions;
 
 public sealed class InvalidExpectedAccessException : InvalidOperationException;
 
+public static class UnexpectedFunction {
+   [MethodImpl(AggressiveInlining)]
+   public static Unexpected<E> Unexpected<E>(scoped in E error) => new(error);
+}
+
 public readonly ref struct Unexpected<E>
 where E : allows ref struct {
    public E Error { get; }
